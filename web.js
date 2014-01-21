e4d46bb030b6a87fbe45f3f99375aea62a204cb0
Seth Clark <seth.e.clark@gmail.com>
2014-01-21 17:11:41 -0500

    Fixed listening
    Removed the hardcoded port '3000' listening

--- web.js
+++ web.js
@@ -58,13 +58,9 @@ app.get('/registry', function (req, res) {
   );
 });
 
-app.listen(3000);
+var port = Number(process.env.PORT || 3000);
+app.listen(port, function(){
+	console.log("Listening on " + port);
+});
 
-// app.get('/', function(request, response) {
-  // response.send('Hello World!');
-// });
-// 
-// var port = process.env.PORT || 5000;
-// app.listen(port, function() {
-  // console.log("Listening on " + port);
-// });
+
